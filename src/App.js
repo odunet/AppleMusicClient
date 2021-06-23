848858f42e8b2c7a95e08cc39ed58142b5fe2b77
odunet <odunet2000@yahoo.com>
2021-06-23 23:10:27 +1000

    Added depsndency to useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import './App.css';
 import Header from './components/common/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,26 +10,27 @@ function App() {
   //Bring in dispatch
   const { dispatch } = useContext(store);
 
+  let musicUrl = 'https://itunes.apple.com/us/rss/topsongs/limit=100/json';
+  let albumUrl = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
+
   //get user data | Music
   useEffect(() => {
     async function apiCall() {
-      Promise.all([
-        getData('https://itunes.apple.com/us/rss/topsongs/limit=100/json'),
-        getData('https://itunes.apple.com/us/rss/topalbums/limit=100/json'),
-      ]).then(([song, album]) => {
-        dispatch({
-          type: types.Load_Music,
-          data: song.data.feed.entry,
-        });
-        dispatch({
-          type: types.Load_Album,
-          data: album.data.feed.entry,
-        });
-      });
+      Promise.all([getData(musicUrl), getData(albumUrl)]).then(
+        ([song, album]) => {
+          dispatch({
+            type: types.Load_Music,
+            data: song.data.feed.entry,
+          });
+          dispatch({
+            type: types.Load_Album,
+            data: album.data.feed.entry,
+          });
+        }
+      );
     }
-
     apiCall();
-  }, []);
+  }, [albumUrl, dispatch, musicUrl]);
 
   return (
     <>
